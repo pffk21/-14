98e2f00a77acdaff24fb0b1e733d4f45fa812e73
Paul <gerpaul588@gmail.com>
2024-09-02 16:47:10 +0300

    Initial Commit

---
+++ urok/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  urok
+//
+//  Created by paul blazhko on 02.09.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
